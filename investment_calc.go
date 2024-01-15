ba3f28e45bcc5f2dff46a93adc32fa66a3ee5609
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-15 15:39:06 -0700

    add user input with fmt.Scan

--- investment_calc.go
+++ investment_calc.go
@@ -6,10 +6,24 @@ import (
 )
 
 func main() {
-	investmentAmount, years := 1000.0, 10.0
-	expectedReturnRate := 5.5
+	const inflationRate = 6.5
+
+	var investmentAmount, years, expectedReturnRate float64
+
+	//Get user input with fmt.Scan
+	fmt.Print("Enter the amount you want to invest: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter the number of years you want to invest this cash: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Finally, enter the expected rate of return: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	//Taking inflation into account:
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
